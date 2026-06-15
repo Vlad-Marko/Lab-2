9956cb5d12a56a8d0dc78f9b95d19bb7a9b24bc8
Владислав Дружицький <druzhytskyivlad@gmail.com>
2026-06-15 16:27:26 +0300

    new file:   mysite/polls/static/polls/images/background.jpg

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,27 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-# Register your models here.
+# # Register your models here.
+# from django.contrib import admin
+# from .models import Question, Choice
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Information about date", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
